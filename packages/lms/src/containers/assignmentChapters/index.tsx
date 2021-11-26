b110a5c2a363f49422ae5daa9fc91b8907e2075a
Faizan Saleem <faizansaleem790@gmail.com>
2021-11-26 16:27:39 +0500

    added package-lock

--- packages/lms/src/containers/assignmentChapters/index.tsx
+++ packages/lms/src/containers/assignmentChapters/index.tsx
@@ -403,7 +403,7 @@ const AssignmentChapters: FC = (): JSX.Element => {
                     onClick={refreshAssignmentChapters}
                     data-cy="notebooksSyncButton"
                   >
-                    <img src={SyncIcon} alt="sync icon" /> <span className="btnName"> Syn </span>
+                    <img src={SyncIcon} alt="sync icon" /> <span className="btnName"> Sync </span>
                   </Button>
                 </Grid>
               </Box>
